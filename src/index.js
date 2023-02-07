5b356b8d0ed4de476dec3378f554a9f80c41a025
Mark Coats <alexco1234@gmail.com>
2023-02-07 09:44:42 -0600

    cards 3/4 done

--- src/index.js
+++ src/index.js
@@ -19,16 +19,55 @@ function renderChars() {
   fetch('http://localhost:3000/toys')
     .then(res => res.json())
     .then(toys => {
-      //console.log(toys);
       //get top element for start of card element
+      let topDiv = document.querySelector('#toy-collection'); //start of card collection
       for(index in toys) {
         console.log(toys[index].name);
+        let tmpToyId = toys[index].id; //get id of each toy
         //create a card
         //fill all data in card
         //append card to list
+         let topOfCard = document.createElement('div'); //beginning of each card
+         topOfCard.id = tmpToyId;
+         topOfCard.className = 'card';
+
+         let name = document.createElement('h2');
+         name.textContent = toys[index].name;
+
+         let img = document.createElement('img');
+         img.className = 'toy-avatar';
+         img.src = toys[index].image;
+         img.alt = toys[index].name;
+
+         let likes = document.createElement('p');
+         likes.textContent = toys[index].likes;
+
+         topOfCard.append(name);
+         topOfCard.append(img);
+         topOfCard.append(likes);
+
+
+        // const nameDiv = document.createElement('div');
+        // nameDiv.id = 'name';
+        // nameDiv.textContent = toys[index].name;
+
+        // const imageDiv = document.createElement('div');
+        // imageDiv.id = 'image';
+        // imageDiv.className = 'toy-avatar';
+        // imageDiv.innerHTML = `<img src='${toys[index].image}' alt='${toys[index].name}'>`;
+
+        // const likesDiv = document.createElement('div');
+        // likesDiv.id = 'likes';
+        // likesDiv.textContent = toys[index].likes;
+        
+        // //append values to card
+        // topOfCard.append(nameDiv);
+        // topOfCard.append(imageDiv);
+        // topOfCard.append(likesDiv);
+        
+        topDiv.append(topOfCard);
       }
       //append card list to start of card element
-      //for(let i = 0; i < toys.length; i++){}
     })
 }
 
