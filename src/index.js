f20cc84279b0d886d5b5bc9bd196a109beb321b3
Mark Coats <alexco1234@gmail.com>
2023-02-07 08:57:30 -0600

    mark

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  renderChars();
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +14,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function renderChars() {
+  fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => {
+      //console.log(toys);
+      //get top element for start of card element
+      for(index in toys) {
+        console.log(toys[index].name);
+        //create a card
+        //fill all data in card
+        //append card to list
+      }
+      //append card list to start of card element
+      //for(let i = 0; i < toys.length; i++){}
+    })
+}
+
+function pushChars() {
+
+}
